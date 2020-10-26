c9ac68c9696bc6f7209726bfb2aebc9f44005db9
Sumbo Francisco <Sumbo.Francisco@gmail.com>
2020-10-26 23:27:47 +0100

    first commit

---
+++ src/test/java/com/example/Spring/Bookzy/SpringBookzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring.Bookzy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBookzyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
