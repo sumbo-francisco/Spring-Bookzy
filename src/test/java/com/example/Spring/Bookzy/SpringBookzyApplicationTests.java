c458f178adad1461f4db7d8fb648ae8eeefa1575
Sumbo Francisco <Sumbo.Francisco@gmail.com>
2020-11-02 21:21:36 +0100

    This is a commit to push the Junit tests

--- src/test/java/com/example/Spring/Bookzy/SpringBookzyApplicationTests.java
+++ src/test/java/com/example/Spring/Bookzy/SpringBookzyApplicationTests.java
@@ -1,11 +1,25 @@
 package com.example.Spring.Bookzy;
 
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+//import com.simplilearn.workshop.HelloWorld;
 
 @SpringBootTest
 class SpringBookzyApplicationTests {
 
+	@Test
+	public void testSpringBookzyConstructor() {
+		try{
+			new SpringBookzyApplication();
+			}catch (Exception e) {
+				fail("Construction Failed!");
+			}
+	}
+	
 	@Test
 	void contextLoads() {
 	}
